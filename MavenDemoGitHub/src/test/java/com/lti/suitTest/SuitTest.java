67ba0157fa6b448a5f0b95490b7f337afe3bf212
Zaid Khan <40146675+zk-146@users.noreply.github.com>
2022-07-14 12:53:20 +0530

    Pull test

--- MavenDemoGitHub/src/test/java/com/lti/suitTest/SuitTest.java
+++ MavenDemoGitHub/src/test/java/com/lti/suitTest/SuitTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.junit.platform.suite.api.SelectPackages;
+//
+//
 
 @SelectPackages("com.lti.demo")
 class SuitTest {
